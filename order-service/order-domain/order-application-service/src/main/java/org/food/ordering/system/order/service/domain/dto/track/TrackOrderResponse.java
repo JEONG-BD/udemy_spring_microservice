c84d9e34611bcf88dcec4e5a3dfd3c9687bb2f81
JEONG-BD <dev.byeongdu@gmail.com>
2025-10-07 22:08:41 +0900

    tracking dto

---
+++ order-service/order-domain/order-application-service/src/main/java/org/food/ordering/system/order/service/domain/dto/track/TrackOrderResponse.java
@@ -0,0 +1,23 @@
+package org.food.ordering.system.order.service.domain.dto.track;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.food.ordering.system.domain.valuobject.OrderStatus;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@AllArgsConstructor
+public class TrackOrderResponse {
+
+    @NotNull
+    private final UUID orderTrackingId;
+
+    @NotNull
+    private final OrderStatus orderStatus;
+    private final List<String> failureMessage;
+
+}
